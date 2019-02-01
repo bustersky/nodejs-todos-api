20e4ef84dc4091c76a57e29a8317d076153b865b
Marcin Korecki <marcin.korecki@orba.co>
2019-02-01 16:24:11 +0100

    Added POST /todos route and refactored mongoose

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+   var todo = new Todo({
+      text: req.body.text
+   });
+
+   todo.save().then((doc) => {
+      res.send(doc);
+   }, (err) => {
+      res.status(400).send(err);
+   });
+});
+
+// app.get('/todos', () => {
+//
+// });
+
+app.listen(port, () => {
+   console.log(`Started on port ${port}`);
+});
